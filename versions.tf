2924d9393947a7248a7f634bd0c389c869cd215e
RithwikksEY <Rithwik.Kumar.Sahu@gds.ey.com>
2026-02-25 09:38:17 +0530

    Add files via upload

--- versions.tf
+++ versions.tf
@@ -1,4 +1,21 @@
 terraform {
+  cloud {
+    organization = "Terraform-Learning-Rithwik"
+ 
+    workspaces {
+      name = "terraform_demo_azure"
+    }
+  }
+ 
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+terraform {
   required_version = ">= 1.5.0"
  
   required_providers {
